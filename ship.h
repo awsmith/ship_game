adbe8df24e54bc1667f9fe4a72e5f86c39cb438a
foest <foest88@gmail.com>
2013-05-16 18:16:46 -0700

    add include <string>

--- ship.h
+++ ship.h
@@ -1,6 +1,7 @@
 // Header file for ship class
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
+#include <string>
 
 #ifndef SHIP_H
 #define SHIP_H
@@ -10,48 +11,48 @@ class ship
   public:
   		// Constructor
 		ship();
-		
+
 		// Controls
 		void handle_input();
-		
+
 		// Move ship and hitbox
 		void move();
-		
+
 		// Apply ship to a surface
 		void show();
-		
+
 		// Return coordinates
 		const SDL_Rect get_xy();
-		
+
 		// Return hitbox
 		const SDL_Rect get_hitBox();
-		
+
 		// Return health remaining
 		const int get_hp();
-		
+
 	private:
 		// Coordinates
 		SDL_Rect offset;
-		
+
 		// Hitbox
 		SDL_Rect hitBox;
-		
+
 		// Orientation of ship
 		int status;
-		
+
 		// Velocity
 		int v_x;
 		int v_y;
-		
+
 		// Image file
 		std::string filename;
-		
+
 		// Current image frame
 		int frame;
-		
+
 		// Health points of ship
 		int hp;
-		
+
 };
 
 #endif
