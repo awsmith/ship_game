baf37574663545ca5288013eb7cdcb3155889fa9
awsmith <merryarronoftheshire@gmail.com>
2013-05-20 22:26:30 -0700

    Update ship.h

--- ship.h
+++ ship.h
@@ -19,7 +19,9 @@ class ship
 		void move();
 
 		// Apply ship to a surface
-		void show();
+		/* If show calls apply_surface from utils.h there is no reference to screen outside of main
+		   Pass screen as arugment to show()? */
+		void show(SDL_Surface *destination);
 
 		// Return coordinates
 		const SDL_Rect get_xy();
@@ -45,6 +47,8 @@ class ship
 		int v_y;
 	
 		// Surface for class image
+		/* If surfaces are local to their class there's no need to deal with the surface manually
+		   Maybe? */
 		SDL_Surface* image;
 		
 		// Image file
