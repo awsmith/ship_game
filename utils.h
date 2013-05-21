888d0e6645064f29b8272e5bee6975203bbcb3a5
foest <foest88@gmail.com>
2013-05-20 17:14:30 -0700

    add utils.h

---
+++ utils.h
@@ -0,0 +1,15 @@
+//Header file for utility functions
+
+
+#ifndef UTILS_H_INCLUDED
+#define UTILS_H_INCLUDED
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include <string>
+
+void apply_surface(int x, int y, SDL_Surface *source, SDL_Surface *destination, SDL_Rect *clip = NULL);
+
+SDL_Surface *load_image(std::string filename);
+
+#endif
