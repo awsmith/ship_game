48e78c02055c1ea945bf7f53f7f1140b2933860e
awsmith <merryarronoftheshire@gmail.com>
2013-05-26 23:20:27 -0700

    Added Timer class and fps instance

--- game.cpp
+++ game.cpp
@@ -5,17 +5,52 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32;
-
+const int FRAMES_PER_SECOND = 20;
 SDL_Surface *screen = NULL;
 SDL_Event event;
+
+
+// Classes
+class Timer
+{
+  public:
+// Default constructor
+  Timer();
+
+	// The various clock actions
+	void start();
+	void stop();
+	void pause();
+	void unpause();
+
+	// Get the timer's time
+	int get_ticks();
+
+	// Checks the status of the timer
+	bool is_started();
+	bool is_paused();
+
+	private:
+	// The time when the timer startered
+	int startTicks;
+
+	// The ticks stored when the timer was paused
+	int pausedTicks;
+
+	// The timer status
+	bool paused;
+	bool started;
+};
 
 int main()
 {
 	SDL_Event event;	
-
 	bool quit = false;
 
 	screen = init(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, screen);
+
+	Timer fps;
+	fps.start();
 
 	if(screen == NULL)
 	{
@@ -32,10 +67,8 @@ int main()
 				quit = true;
 			}
 		}	
-		
 		// Make the screen white	
 		SDL_FillRect( screen, &screen->clip_rect, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
-
 		// Apply player and generated prjoectiles
 
 		// Apply enemy and generated projectiles
@@ -44,8 +77,65 @@ int main()
 		{
 			return 0;
 		}
+		
+		if(fps.get_ticks() < 1000 / FRAMES_PER_SECOND)
+		{
+			SDL_Delay((1000 / FRAMES_PER_SECOND) - fps.get_ticks());
+		}
 	}
 	SDL_Quit;
 	return 0;
 }
 
+
+Timer::Timer()
+{
+	// Initialize the variables
+	startTicks = 0;
+	pausedTicks = 0;
+	paused = false;
+	started = false;
+}
+
+void Timer::start()
+{
+	// Start the timer
+	started = true;
+
+	// Unpause the timer
+	paused = false;
+
+	// Get the current clock time
+	startTicks =SDL_GetTicks();
+}
+
+void Timer::stop()
+{
+	// Stop the timer
+	started = false;
+
+	// Unpause the timer
+	paused = false;
+
+}
+
+int Timer::get_ticks()
+{
+	// If the timer is running
+	if(started == true)
+	{
+		// If the timer is paused
+		if(paused == true)
+		{
+			// Return the number of ticks when the timer was paused
+			return pausedTicks;
+		}
+		else
+		{
+			// Return the current time minus the start time
+			return SDL_GetTicks() - startTicks;
+		}
+
+	}
+}
+	// If the timer isn't running
