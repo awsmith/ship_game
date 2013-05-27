c79aacccdd3161346fa5264757fc28a0f7fe0f48
foest <foest88@gmail.com>
2013-05-27 16:04:27 -0700

    change ship move speed

--- ship.cpp
+++ ship.cpp
@@ -48,10 +48,10 @@ void ship::handle_input(SDL_Event &event)
 	{
 		switch(event.key.keysym.sym)
 		{
-			case SDLK_UP: v_y -= hitBox.h / 2; break;
-			case SDLK_DOWN: v_y += hitBox.h / 2; break;
-			case SDLK_LEFT: v_x -= hitBox.w / 2; break;
-			case SDLK_RIGHT: v_x += hitBox.w / 2; break;
+			case SDLK_UP: v_y -= hitBox.h / 4; break;
+			case SDLK_DOWN: v_y += hitBox.h / 4; break;
+			case SDLK_LEFT: v_x -= hitBox.w / 4; break;
+			case SDLK_RIGHT: v_x += hitBox.w / 4; break;
 		}
 	}
 
@@ -59,10 +59,10 @@ void ship::handle_input(SDL_Event &event)
 	{
 		switch(event.key.keysym.sym)
 		{
-			case SDLK_UP: v_y += hitBox.h / 2; break;
-			case SDLK_DOWN: v_y -= hitBox.h / 2; break;
-			case SDLK_LEFT: v_x += hitBox.w / 2; break;
-			case SDLK_RIGHT: v_x -= hitBox.w / 2; break;
+			case SDLK_UP: v_y += hitBox.h / 4; break;
+			case SDLK_DOWN: v_y -= hitBox.h / 4; break;
+			case SDLK_LEFT: v_x += hitBox.w / 4; break;
+			case SDLK_RIGHT: v_x -= hitBox.w / 4; break;
 		}
 	}
 }
