538b391ee9fd2353392112ac0f12b74c5b097c36
awsmith <merryarronoftheshire@gmail.com>
2013-05-26 23:37:40 -0700

    Updated constructor and calls to offset

--- ship.cpp
+++ ship.cpp
@@ -10,13 +10,22 @@
 // Constructor
 ship::ship()
 {
-        offset.x = 0;
-	offset.y = 0;
+	
+        offset[0].x = 0;
+	offset[0].y = 0;
+	offset[0].w = 37;
+	offset[0].h = 32;
+
+        offset[1].x = 0;
+	offset[1].y = 40;
+	offset[1].w = 37;
+	offset[1].h = 32;
+
 	status = 0;
 	v_x = 0;
 	v_y = 0;
-	image = NULL;
 	filename = "ship.png";
+	image = NULL;
 	frame = 0;
 	hp = 1;
 	hitBox.x = 0;
@@ -34,6 +43,7 @@ ship::~ship()
 
 // Controls
 void ship::handle_input(SDL_Event &event)
+{
 {
 	if(event.type == SDL_KEYDOWN)
 	{
@@ -62,21 +72,21 @@ void ship::handle_input(SDL_Event &event)
 // TODO: check screen edges, check collisions(?)
 void ship::move()
 {
-	offset.x += v_x;
+//	offset.x += v_x;
 	hitBox.x += v_x;
 
-	offset.y += v_y;
+//	offset.y += v_y;
 	hitBox.y += v_y;
 
 	if(false)
 	{
-		offset.x -= v_x;
+//		offset.x -= v_x;
 		hitBox.x -= v_x;
 	}
 
 	if(false) //  wat, both if statements will execute
 	{
-		offset.y -= v_y;
+//		offset.y -= v_y;
 		hitBox.y -= v_y;
 	}
 }
@@ -84,13 +94,14 @@ void ship::move()
 // Apply ship to a surface
 void ship::show(SDL_Surface *destination)
 {
-	apply_surface(offset.x, offset.y, image, destination);
+	apply_surface(hitBox.x, hitBox.y, image, destination, &offset[frame]);
+
 }
 
 // Return coordinates
 const SDL_Rect ship::get_xy()
 {
-	return offset;
+	return hitBox;
 }
 
 // Return hitbox
