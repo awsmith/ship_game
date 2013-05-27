75c64a7c8f5ee5df5672178c2fb43f80bc89f740
foest <foest88@gmail.com>
2013-05-27 16:02:27 -0700

    change fps

--- game.cpp
+++ game.cpp
@@ -6,7 +6,7 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32;
-const int FRAMES_PER_SECOND = 10;
+const int FRAMES_PER_SECOND = 30;
 
 // Game surfaces
 SDL_Surface *screen = NULL;
