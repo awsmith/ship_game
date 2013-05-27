f7279fb3337644ef8b3f7d49675b8a616462c5df
awsmith <merryarronoftheshire@gmail.com>
2013-05-27 10:49:05 -0700

    Cleaned and commented code

--- game.cpp
+++ game.cpp
@@ -2,15 +2,20 @@
 #include "utils.h"
 #include "ship.h"
 
+// Game constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32;
-const int FRAMES_PER_SECOND = 20;
+const int FRAMES_PER_SECOND = 10;
+
+// Game surfaces
 SDL_Surface *screen = NULL;
+
+// Events
 SDL_Event event;
 
-
 // Classes
+// TODO: Remove timer from game.cpp and creater Timer header and implementation file
 class Timer
 {
   public:
@@ -44,45 +49,60 @@ class Timer
 
 int main()
 {
-	SDL_Event event;	
+	// Quit flag for event loop
 	bool quit = false;
 
+	// Initialize SDL systems and screen
+	// TODO: Change init function to bool and pass screen as a reference to a pointer
 	screen = init(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, screen);
 
-	ship myShip;
-	Timer fps;
-	fps.start();
-
+	// If init failed to initialize everything
 	if(screen == NULL)
 	{
 		return 0;
 	}
 
+	// Class instances
+	ship myShip;
+	Timer fps;
+
+	// While the user has not quit
 	while(quit == false)
 	{
+		// Start the fps timer
+		fps.start();
+
 		while(SDL_PollEvent(&event))
 		{
-			// ship input
+			// Handle input from user. Only accepts up, down, left and right keys currently. 
+			myShip.handle_input(event);
+
+			// If the user X's out the window
 			if(event.type == SDL_QUIT)
 			{
+				// Exit event loop
 				quit = true;
 			}
-			myShip.handle_input(event);
 		}	
+
 		// Make the screen white	
 		SDL_FillRect( screen, &screen->clip_rect, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
-	//	apply_surface(0, 0, myShip.image, screen);
+
+		// Adjust the x and y coordinates of the ship and apply to screen	
 		myShip.move();
 		myShip.show(screen);
-		// Apply player and generated prjoectiles
 
-		// Apply enemy and generated projectiles
+		// TODO: Apply player and generated prjoectiles
 
+		// TODO: Apply enemy and generated projectiles
+
+		// Refresh screen	
 		if(SDL_Flip(screen) == -1)
 		{
 			return 0;
 		}
-		
+	
+		// Cap the frame rate if game is running too fast	
 		if(fps.get_ticks() < 1000 / FRAMES_PER_SECOND)
 		{
 			SDL_Delay((1000 / FRAMES_PER_SECOND) - fps.get_ticks());
@@ -92,7 +112,7 @@ int main()
 	return 0;
 }
 
-
+// TODO: Remove once Timer header and implementation files have been created
 Timer::Timer()
 {
 	// Initialize the variables
@@ -142,5 +162,45 @@ int Timer::get_ticks()
 		}
 
 	}
-}
 	// If the timer isn't running
+	return 0;
+}
+
+void Timer::pause()
+{
+	// If the timer is running and isn't already paused
+	if((started == true) && (paused == false))
+	{
+		// Pause the timer
+		paused = true;
+
+		// Calculate the paused ticks
+		pausedTicks = SDL_GetTicks() - startTicks;
+	}
+}
+
+void Timer::unpause()
+{
+	// If the timer is paused
+	if(paused == true)
+	{
+		// Unpaused the timer
+		paused = false;
+
+		// Reset the starting ticks
+		startTicks = SDL_GetTicks() - pausedTicks;
+
+		// Reset the paused ticks
+		pausedTicks = 0;
+	}
+}
+
+bool Timer::is_started()
+{
+	return started;
+}
+
+bool Timer::is_paused()
+{
+	return paused;
+} 
