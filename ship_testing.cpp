bc206f27089a458f00920c94688c7b728a5638b6
foest <foest88@gmail.com>
2013-06-07 23:07:17 -0700

    add ship_testing.cpp

---
+++ ship_testing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "utils.h"
+#include "Ship.h"
+
+int main()
+{
+  Ship tempShip;
+  //SDL_Rect tempCoords = tempShip.get_coords();
+  //std::cout << "tempShip coords:\nX: " << tempCoords.x;
+  //std::vector<Ship> ships;
+
+  return 0;
+}
