a797c5d31e17e22c969588d02ba2d2917adfaa1c
awsmith <merryarronoftheshire@gmail.com>
2013-06-09 23:42:54 -0700

    Commented code

--- Projectile.cpp
+++ Projectile.cpp
@@ -2,79 +2,99 @@
 #include "utils.h"
 #include <iostream>
 
-
+// Default constructor
 Projectile::Projectile(SDL_Rect origin)
 {
-  offset[0].x = 0;
-  offset[0].y = 0;
-  offset[0].w = 37;
-  offset[0].h = 32;
+	// Clip first image for projectile animation
+	offset[0].x = 0;
+	offset[0].y = 0;
+	offset[0].w = 37;
+	offset[0].h = 32;
+	// Clip second image for projectile animation
+	offset[1].x = 40;
+	offset[1].y = 0;
+	offset[1].w = 37;
+	offset[1].h = 32;
 
-  offset[1].x = 40;
-  offset[1].y = 0;
-  offset[1].w = 37;
-  offset[1].h = 32;
+	status = 0;
 
-  status = 0;
-  v_x = 0;
-  v_y = 0;
-  filename = "ship.png";
-  image = load_image(filename);
-  frame = 1;
-  // Projectile spawned at top center of ship
-  hitBox.x = origin.x + (origin.w / 2);
-  hitBox.y = origin.h;
-  hitBox.w = 37;
-  hitBox.h = 32;
+	// Projectile velocities
+	v_x = 0;
+	v_y = 0;
+	
+	// Set image filename and load in
+	filename = "ship.png";
+	image = load_image(filename);
+	frame = 1;
+
+	// Projectile spawned at top center of ship
+	hitBox.x = origin.x + (origin.w / 2);
+	hitBox.y = origin.h;
+	hitBox.w = 37;
+	hitBox.h = 32;
 }
 
+// Destructor
 Projectile::~Projectile()
 {
-  SDL_FreeSurface(image);
+	SDL_FreeSurface(image);
 }
 
+// Return damage for projectile
 const int Projectile::get_damage()
 {
-  return damage;
+	return damage;
 }
 
+// Return the coordinates, width and height of the projectile
 const SDL_Rect Projectile::get_coords()
 {
-  return hitBox;
+	return hitBox;
 }
 
 const SDL_Rect Projectile::get_hitbox()
 {
-  return hitBox;
+	return hitBox;
 }
 
+// Applies the projectile to the screen
 void Projectile::show(SDL_Surface *destination)
 {
-  apply_surface(hitBox.x, hitBox.y, image, destination, &offset[frame]);
-  frame++;
-  if(frame >= 2)
-  {
-    frame = 0;
-  }
+	apply_surface(hitBox.x, hitBox.y, image, destination, &offset[frame]);
+
+	// Increment the frame counter for animation
+	frame++;
+	
+	// Check frame counter boundry
+	if(frame >= 2)
+	{
+		// Reset the frame counter
+		frame = 0;
+	}
 }
 
+// Updates the x, y coordinates based on the projectile velocity
 void Projectile::move()
 {
-  hitBox.x += v_x;
+	// Update x coordinate
+	hitBox.x += v_x;
+	// Update y coordinate
+	hitBox.y += v_y;
 
-  hitBox.y += v_y;
+	// Check if projectile goes out of x bounds
+	if((hitBox.x < 0) || ((hitBox.x + hitBox.w) > 640))
+	{
+		hitBox.x -= v_x;
+	}
 
-  if((hitBox.x < 0) || ((hitBox.x + hitBox.w) > 640))
-  {
-    hitBox.x -= v_x;
-  }
-
-  if((hitBox.y < 0) || ((hitBox.y + hitBox.h) > 480))
-  {
-    hitBox.y -= v_y;
-  }
+	// Check if projectile goes out of y bounds
+	if((hitBox.y < 0) || ((hitBox.y + hitBox.h) > 480))
+	{
+		hitBox.y -= v_y;
+	}
 }
 
+// Returns the image for projectile
 SDL_Surface* Projectile::get_image()
 {
   return image;
