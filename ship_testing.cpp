aa24a3e1f208455fe532f4c9007b8487200a4aa8
foest <foest88@gmail.com>
2013-06-12 16:10:53 -0700

    modify ship_testing to check for memory leaks

--- ship_testing.cpp
+++ ship_testing.cpp
@@ -6,7 +6,39 @@ int main()
 {
   SDL_Surface *screen = NULL;
   screen = init(640, 480, 32, screen);
-  Ship tempShip;
-  std::cout << "tempShip coords:\nX: " << tempShip.get_coords().x << "\nY: " << tempShip.get_coords().y << '\n';
+  SDL_Event event;
+
+  bool quit = false;
+
+  std::vector<Ship*> shipList;
+  std::vector<Ship*>::iterator it;
+
+  while(quit == false)
+  {
+    while(SDL_PollEvent(&event))
+    {
+      std::cout << shipList.size() << std::endl;
+
+      if(shipList.size() == 10)
+      {
+        while(shipList.size() > 0)
+        {
+          it = shipList.begin();
+          delete * it;
+          shipList.erase(it);
+        }
+      }
+      else if(shipList.size() < 10)
+      {
+        shipList.push_back(new Ship());
+      }
+
+      if(event.type == SDL_QUIT)
+      {
+        quit = true;
+      }
+    }
+  }
+
   return 0;
 }
