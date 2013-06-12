f78f1af164130238f5e8369c008db2369dd945ed
awsmith <merryarronoftheshire@gmail.com>
2013-06-12 14:49:36 -0700

    Added destroy member and function to check destroy's status

--- Projectile.h
+++ Projectile.h
@@ -8,39 +8,41 @@
 
 class Projectile
 {
-  public:
-    Projectile(SDL_Rect origin);
-    ~Projectile();
+	public:
+		Projectile(SDL_Rect origin);
+		~Projectile();
 
-    //Move projectile
-    void move();
+		//Move projectile
+		void move();
 
-    //Apply projectile to a surface
-    void show(SDL_Surface *destination);
+		//Apply projectile to a surface
+		void show(SDL_Surface *destination);
 
-    //Return coordinates
-    const SDL_Rect get_coords();
+		//Return coordinates
+		const SDL_Rect get_coords();
 
-    //Return hitbox
-    const SDL_Rect get_hitbox();
+		//Return hitbox
+		const SDL_Rect get_hitbox();
 
-    //Return damage amount
-    const int get_damage();
+		//Return damage amount
+		const int get_damage();
 
-    //Return Projectile surface
-    SDL_Surface *get_image();
+		//Return Projectile surface
+		SDL_Surface *get_image();
 
-    SDL_Surface *image;
+		SDL_Surface *image;
+		bool get_destroy();
 
-  private:
-    int damage;
-    int v_x;
-    int v_y;
-    int frame;
-    int status;
-    SDL_Rect offset[2];
-    SDL_Rect hitBox;
-    std::string filename;
+	private:
+		bool destroy;
+		int damage;
+		int v_x;
+		int v_y;
+		int frame;
+		int status;
+		SDL_Rect offset[2];
+		SDL_Rect hitBox;
+		std::string filename;
 };
 
 #endif
